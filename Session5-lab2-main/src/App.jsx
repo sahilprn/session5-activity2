57ba7b06cc1422c349f4e8e3b2c9ec9c28e168de
sahilpreet singh <sahilpreetsingh84884@gmail.com>
2024-10-12 12:03:05 +0300

    update

---
+++ Session5-lab2-main/src/App.jsx
@@ -0,0 +1,14 @@
+// App.jsx
+import Hello from './Hello';
+import Bye from './Bye';
+// App.jsx
+function App() {
+  return (
+    <div>
+      <Hello />
+      <Bye />
+    </div>
+  );
+}
+
+export default App;
